2a6a4295201bfa43588a260d22f00a64f52c69cf
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-09 17:32:39 +0000

    Remove testing calls

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,5 @@ use backend::*;
 fn main() {
     let mut backend = SectionManager::load_from_file();
 
-    println!("{}", backend.add_section("Todoing2"));
-    println!("{}", backend.select_section("Todoing2"));
-
-    println!("{:?}", backend);
-
     backend.save_to_file();
 }
