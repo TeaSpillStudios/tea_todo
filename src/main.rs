51a73828004be5369c0aa8c8d64f4aecb03a3ea9
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-09 18:17:12 +0000

    Integrate cli.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod backend;
 use backend::*;
 
+mod cli;
+use cli::*;
+
 fn main() {
     let mut backend = SectionManager::load_from_file();
+
+    cli_manager::start(&mut backend);
 
     backend.save_to_file();
 }
