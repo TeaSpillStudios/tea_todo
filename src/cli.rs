9e92c434afceb0d3706ea8ae552b977f8ecb6597
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-10 00:08:34 +0000

    RustFMT

--- src/cli.rs
+++ src/cli.rs
@@ -16,37 +16,37 @@ pub mod cli_manager {
                     .about("Add a new task")
                     .arg(arg!(<Name> "The name of the task"))
                     .arg(arg!(<Description> "The description of the task"))
-                    .arg_required_else_help(true)
+                    .arg_required_else_help(true),
             )
             .subcommand(
                 Command::new("remove")
                     .about("Remove a task")
                     .arg(arg!(<Name> "The name of the task"))
-                    .arg_required_else_help(true)
+                    .arg_required_else_help(true),
             )
             .subcommand(
                 Command::new("add_section")
                     .about("Add a section")
                     .arg(arg!(<Name> "The name of the section to add"))
-                    .arg_required_else_help(true)
+                    .arg_required_else_help(true),
             )
             .subcommand(
                 Command::new("remove_section")
                     .about("Remove a section")
                     .arg(arg!(<Name> "The name of the section to remove"))
-                    .arg_required_else_help(true)
+                    .arg_required_else_help(true),
             )
             .subcommand(
                 Command::new("select_section")
                     .about("Select a section")
                     .arg(arg!(<Name> "The name of the section to select"))
-                    .arg_required_else_help(true)
+                    .arg_required_else_help(true),
             )
             .subcommand(
                 Command::new("set_completed")
                     .about("Set whether a task is completed or not")
                     .arg(arg!(<Name> "The name of the task to change"))
-                    .arg_required_else_help(true)
+                    .arg_required_else_help(true),
             )
             .subcommand(Command::new("list_sections").about("List out all the sections"));
 
@@ -54,19 +54,31 @@ pub mod cli_manager {
             Some(("list", _sub)) => {
                 if !backend.map.contains_key(&backend.current_section) {
                     println!("{}", "The current section does not exist! Please make a new section and select it.".red());
-                    return
+                    return;
                 }
 
-                println!("Section {}", if backend.is_section_completed() { backend.current_section.green() } else { backend.current_section.red() });
+                println!(
+                    "Section {}",
+                    if backend.is_section_completed() {
+                        backend.current_section.green()
+                    } else {
+                        backend.current_section.red()
+                    }
+                );
                 for task in backend.get_tasks() {
-                    println!("  {} - {}", if task.1.completed { task.0.green() } else { task.0.red() }, task.1.description);
+                    println!(
+                        "  {} - {}",
+                        if task.1.completed {
+                            task.0.green()
+                        } else {
+                            task.0.red()
+                        },
+                        task.1.description
+                    );
                 }
             }
             Some(("add", sub)) => {
-                let name = sub
-                    .get_one::<String>("Name")
-                    .map(|s| s.as_str())
-                    .unwrap();
+                let name = sub.get_one::<String>("Name").map(|s| s.as_str()).unwrap();
 
                 let description = sub
                     .get_one::<String>("Description")
@@ -76,46 +88,31 @@ pub mod cli_manager {
                 println!("{}", backend.add_task(name, description));
             }
             Some(("remove", sub)) => {
-                let name = sub
-                    .get_one::<String>("Name")
-                    .map(|s| s.as_str())
-                    .unwrap();
+                let name = sub.get_one::<String>("Name").map(|s| s.as_str()).unwrap();
 
                 println!("{}", backend.remove_task(name));
             }
             Some(("add_section", sub)) => {
-                let name = sub
-                    .get_one::<String>("Name")
-                    .map(|s| s.as_str())
-                    .unwrap();
+                let name = sub.get_one::<String>("Name").map(|s| s.as_str()).unwrap();
 
                 println!("{}", backend.add_section(name));
             }
             Some(("remove_section", sub)) => {
-                let name = sub
-                    .get_one::<String>("Name")
-                    .map(|s| s.as_str())
-                    .unwrap();
+                let name = sub.get_one::<String>("Name").map(|s| s.as_str()).unwrap();
 
                 println!("{}", backend.remove_section(name));
             }
             Some(("select_section", sub)) => {
-                let name = sub
-                    .get_one::<String>("Name")
-                    .map(|s| s.as_str())
-                    .unwrap();
+                let name = sub.get_one::<String>("Name").map(|s| s.as_str()).unwrap();
 
                 println!("{}", backend.select_section(name));
             }
             Some(("set_completed", sub)) => {
-                let name = sub
-                    .get_one::<String>("Name")
-                    .map(|s| s.as_str())
-                    .unwrap();
+                let name = sub.get_one::<String>("Name").map(|s| s.as_str()).unwrap();
 
                 if !backend.map.contains_key(&backend.current_section) {
                     println!("{}", "The current section does not exist! Please make a new section and select it.".red());
-                    return
+                    return;
                 }
 
                 let result: bool = loop {
@@ -139,8 +136,15 @@ pub mod cli_manager {
             }
             Some(("list_sections", _sub)) => {
                 for section in &backend.map {
-                    println!("Section {}", if backend.is_section_completed_unsafe(section.0) { section.0.green() } else { section.0.red() });
-                };
+                    println!(
+                        "Section {}",
+                        if backend.is_section_completed_unsafe(section.0) {
+                            section.0.green()
+                        } else {
+                            section.0.red()
+                        }
+                    );
+                }
             }
             _ => println!("Not implemented yet."),
         }
